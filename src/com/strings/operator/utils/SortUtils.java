984d83f008402f4a824e13bf2d3a6d6a67f7a325
vetaveta <v.demerchyan@gmail.com>
2021-07-12 09:27:30 +0400

    add quickSort

--- src/com/strings/operator/utils/SortUtils.java
+++ src/com/strings/operator/utils/SortUtils.java
@@ -3,16 +3,35 @@ package com.strings.operator.utils;
 public class SortUtils {
     private SortUtils() {
     }
-
-    public static void insertionSort(char[] array) {
-        for (int i = 1; i < array.length; i++) {
-            char current = array[i];
-            int j = i;
-            while (j > 0 && array[j - 1] > current) {
-                array[j] = array[j - 1];
-                j--;
-            }
-            array[j] = current;
+    public static void quickSort(char[] arr, int first, int last) {
+        if (first < last) {
+            char dividerIndex = (char) partition(arr, first, last);
+            quickSort(arr, first, (char) (dividerIndex - 1));
+            quickSort(arr, dividerIndex, last);
         }
+    }
+    private static int partition(char[] arr, int first, int last) {
+        int rightIndex = last;
+        int leftIndex = first;
+        char pivot = arr[first];
+        while (leftIndex <= rightIndex) {
+            while (arr[leftIndex] < pivot) {
+                leftIndex++;
+            }
+            while (arr[rightIndex] > pivot) {
+                rightIndex--;
+            }
+            if (leftIndex <= rightIndex) {
+                swap(arr, rightIndex, leftIndex);
+                leftIndex++;
+                rightIndex--;
+            }
+        }
+        return leftIndex;
+    }
+    private static void swap(char[] arr, int left, int right) {
+        char temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
     }
 }
--- src/com/strings/operator/utils/StringUtils.java
+++ src/com/strings/operator/utils/StringUtils.java
@@ -16,8 +16,8 @@ public class StringUtils {
     public static boolean isAnagram(String a, String b) {
         char[] aChars = a.toLowerCase().toCharArray();
         char[] bChars = b.toLowerCase().toCharArray();
-        SortUtils.insertionSort(aChars);
-        SortUtils.insertionSort(bChars);
+        SortUtils.quickSort(aChars, 0, aChars.length - 1);
+        SortUtils.quickSort(bChars, 0, bChars.length - 1);
         return Arrays.equals(aChars, bChars);
     }
 }
